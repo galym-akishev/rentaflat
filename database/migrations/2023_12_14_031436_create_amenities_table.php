623002f23627739f9ccaa6048e5ea7c10874a793
Galym Akishev <galymakishevkz@gmail.com>
2023-12-15 10:44:18 +0600

    Added migrations for Advertisements, Amenities, many-to-many

--- database/migrations/2023_12_14_031436_create_amenities_table.php
+++ database/migrations/2023_12_14_031436_create_amenities_table.php
@@ -4,16 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('advertisements', function (Blueprint $table) {
+        Schema::create('amenities', function (Blueprint $table) {
             $table->id();
-            $table->string('title')->nullable(false);
-            $table->string('description')->nullable(false);
+            $table->string('title')
+                ->unique()
+                ->nullable(false);
             $table->timestamps();
 
             $table->softDeletes();
@@ -25,6 +27,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('advertisements');
+        Schema::dropIfExists('amenities');
     }
 };
---
+++ database/migrations/2023_12_15_041920_create_advertisement_amenities_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('advertisement_amenities', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('advertisement_id');
+            $table->unsignedBigInteger('amenity_id');
+
+            $table->index('advertisement_id', 'advertisement_amenity_advertisement_idx');
+            $table->index('amenity_id', 'advertisement_amenity_amenity_idx');
+
+            $table->foreign('advertisement_id')
+                ->on('advertisements')
+                ->references('id');
+            $table->foreign('amenity_id')
+                ->on('amenities')
+                ->references('id');
+
+            $table->timestamps();
+
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('advertisement_amenities');
+    }
+};
