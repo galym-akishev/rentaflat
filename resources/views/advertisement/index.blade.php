00fb60248591f59bdaafb54f5df2e6a8e275ae2e
Galym Akishev <galymakishevkz@gmail.com>
2023-12-18 16:49:58 +0600

    Details of the advertisement page done

--- resources/views/advertisement/index.blade.php
+++ resources/views/advertisement/index.blade.php
@@ -2,28 +2,29 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <ul class="list-group my-2">
-                @foreach ($advertisements as $advertisement)
-                    <li class="list-group-item flex-column justify-content-between align-items-center">
-                        <div>
-                            <div class="fw-bold">{{ $advertisement->title }}</div>
-                            <div class="opacity-50">
-                                <small>
-                                    <a
-                                        class="link-info link-offset-2 link-underline-opacity-0 link-underline-opacity-100-hover"
-                                        href="{{ route('advertisement.show', $advertisement->id) }}">
-                                        [Details]
-                                    </a>
-                                </small>
-                                <small class="text-info">[id: {{ $advertisement->id }}]</small>
+            <div class="col-12">
+                <ul class="list-group my-2">
+                    @foreach ($advertisements as $advertisement)
+                        <li class="list-group-item flex-column justify-content-between align-items-center">
+                            <div>
+                                <div class="fw-bold">{{ $advertisement->title }}</div>
+                                <div class="opacity-50">
+                                    <small>
+                                        <a
+                                            class="link-info link-offset-2 link-underline-opacity-0 link-underline-opacity-100-hover"
+                                            href="{{ route('advertisement.show', $advertisement->id) }}">
+                                            [Details of ID: {{ $advertisement->id }}]
+                                        </a>
+                                    </small>
+                                </div>
                             </div>
-                        </div>
-                        <div>
-                            {{ $advertisement->description }}
-                        </div>
-                    </li>
-                @endforeach
-            </ul>
+                            <div>
+                                {{ $advertisement->description }}
+                            </div>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
         </div>
     </div>
 @endsection
--- resources/views/advertisement/show.blade.php
+++ resources/views/advertisement/show.blade.php
@@ -1,32 +1,38 @@
 @extends('layouts.main')
 @section('content')
-
-<div class="container">
-    <div class="row">
-        <h1>Specific advertisement:</h1>
-        <div>
-            ID: {{ $advertisement->id }}
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>Details of the advertisement:</h1>
+                <div class="card">
+                    <img class="card-img-top" style="width: 3rem;" src="{{ Vite::asset('resources/images/favicon.png') }}" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Title: {{ $advertisement->title }}</h5>
+                        <p class="card-text">Description: {{ $advertisement->description }}</p>
+                        <div class="d-flex">
+                            <a
+                                href="{{ route('advertisement.edit', $advertisement->id) }}"
+                                class="btn btn-info mb-1 me-1">
+                                Edit
+                            </a>
+                            <form action="{{ route('advertisement.delete', $advertisement->id) }}" method="post">
+                                @csrf
+                                @method('delete')
+                                <input
+                                    type="submit"
+                                    class="btn btn-info mb-1 me-1"
+                                    value="Delete">
+                            </form>
+                            <a
+                                href="{{ route('advertisement.index') }}"
+                                class="btn btn-info mb-1 me-1"
+                            >
+                                Back
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div>
-            Title: {{ $advertisement->title }}
-        </div>
-        <div>
-            Description: {{ $advertisement->description }}
-        </div>
     </div>
-    <div>
-        <a href="{{ route('advertisement.edit', $advertisement->id) }}">Edit</a>
-    </div>
-    <div>
-        <form action="{{ route('advertisement.delete', $advertisement->id) }}" method="post">
-            @csrf
-            @method('delete')
-            <input type="submit" value="Delete">
-        </form>
-
-    </div>
-    <div class="row">
-        <a href="{{ route('advertisement.index') }}">Back</a>
-    </div>
-</div>
 @endsection
