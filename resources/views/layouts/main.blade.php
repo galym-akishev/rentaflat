f0cd23b4ebe7d3510cddfd67fb7c7ce52d4e1eae
Galym Akishev <galymakishevkz@gmail.com>
2023-12-18 15:09:21 +0600

    New transition to bootstrap

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,71 +2,48 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-
     <title>Rentaflat - rent a flat</title>
-
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="description" content="Rentaflat - portfolio website on Laravel/PHP">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0">
-
     <link rel="shortcut icon" type="image/x-icon" href="{{ Vite::asset('resources/images/favicon.png') }}"/>
-
     @vite(['resources/scss/app.scss', 'resources/js/app.js'])
-
-    <link rel="stylesheet" href="{{ Vite::asset('resources/plugins/bootstrap/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ Vite::asset('resources/plugins/Ionicons/css/ionicons.min.css') }}">
-    <link rel="stylesheet" href="{{ Vite::asset('resources/plugins/animate-css/animate.css') }}">
-    <link rel="stylesheet" href="{{ Vite::asset('resources/plugins/magnific-popup/magnific-popup.css') }}/">
-    <link rel="stylesheet" href="{{ Vite::asset('resources/plugins/slick/slick.css') }}">
 </head>
 
 <body id="body">
 
-<header class="navigation">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12">
-                <nav class="navbar navbar-expand-lg p-0">
-                    <a class="navbar-brand" href="/">
-                        <img src="{{ Vite::asset('resources/images/logo.png') }}" alt="Logo">
-                    </a>
-                    <button class="navbar-toggler collapsed" type="button" data-toggle="collapse"
-                            data-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded="false"
-                            aria-label="Toggle navigation">
-                        <span class="ion-android-menu"></span>
-                    </button>
-                    <div class="collapse navbar-collapse ml-auto" id="navbar">
-                        <ul class="navbar-nav ml-auto">
-                            <li class="nav-item">
-                                <a class="nav-link" href="/">Home</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('advertisement.index') }}">Advertisements</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('advertisement.create') }}">Create New Ad</a>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
+<div class="container">
+    <div class="row">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand mx-2" href="/">Rentaflat</a>
+            <button
+                class="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarNavDropdown"
+                aria-controls="navbarNavDropdown"
+                aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="/">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('advertisement.index') }}">Listings</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('advertisement.create') }}">Create Ad</a>
+                    </li>
+                </ul>
             </div>
-        </div>
+        </nav>
     </div>
-</header>
+</div>
 
 @yield('content')
-
-<!-- Main jQuery -->
-<script src="{{ Vite::asset('resources/plugins/jquery/jquery.min.js') }}"></script>
-<!-- Bootstrap 3.1 -->
-<script src="{{ Vite::asset('resources/plugins/bootstrap/bootstrap.min.js') }}"></script>
-<!-- slick Carousel -->
-<script src="{{ Vite::asset('resources/plugins/slick/slick.min.js') }}"></script>
-<script src="{{ Vite::asset('resources/plugins/magnific-popup/jquery.magnific-popup.min.js') }}"></script>
-<!-- filter -->
-<script src="{{ Vite::asset('resources/plugins/shuffle/shuffle.min.js') }}"></script>
-<script src="{{ Vite::asset('resources/plugins/SyoTimer/jquery.syotimer.min.js') }}"></script>
-<script src="{{ Vite::asset('resources/js/script.js') }}"></script>
 
 </body>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,14 @@
 @extends('layouts.main')
 @section('content')
-    <section class="main">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <div class="section-title">
-                        <h1>Welcome to Rentaflat!</h1>
-                        <p>Rentaflat is a portfolio project implemented on Laravel/PHP</p>
-                        <p>Rentaflat - a rental properties listings</p>
-                    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="section-title">
+                    <h1>Welcome to Rentaflat!</h1>
+                    <p>Rentaflat is a portfolio project implemented on Laravel/PHP</p>
+                    <p>Rentaflat - a rental properties listings</p>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
 @endsection
