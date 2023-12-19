e0fb7658b4eaa2f745deaf4cce9238297cc9f4f7
Galym Akishev <galymakishevkz@gmail.com>
2023-12-19 08:34:04 +0600

    Create a new advertisement page done

--- resources/views/advertisement/create.blade.php
+++ resources/views/advertisement/create.blade.php
@@ -1,45 +1,43 @@
 @extends('layouts.main')
 @section('content')
-
-<div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-        <form class="max-w-md mx-auto" action="{{ route('advertisement.store') }}" method="post">
-            @csrf
-            <div class="grid md:grid-cols-1 md:gap-6">
-                <div class="relative z-0 w-full mb-5 group">
-                    <input
-                        type="text"
-                        name="title"
-                        id="title"
-                        class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                        placeholder=" "
-                        required />
-                    <label for="title"
-                           class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
-                        Advertisement title
-                    </label>
-                </div>
-                <div class="relative z-0 w-full mb-5 group">
-                    <input
-                        type="text"
-                        name="description"
-                        id="description"
-                        class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                        placeholder=" "
-                        required />
-                    <label for="description"
-                           class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
-                        Advertisement description
-                    </label>
-                </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-wrap">
+                <h1>Create a new advertisement:</h1>
+                <form action="{{ route('advertisement.store') }}" method="post">
+                    @csrf
+                    <div class="form-row">
+                        <label for="title"><b>Title:</b></label>
+                        <input
+                            type="text"
+                            name="title"
+                            id="title"
+                            class="form-control"
+                            placeholder=" "
+                            required/>
+                    </div>
+                    <div class="form-row">
+                        <label for="description"><b>Description:</b></label>
+                        <input
+                            type="text"
+                            name="description"
+                            id="description"
+                            class="form-control"
+                            placeholder=" "
+                            required/>
+                    </div>
+                    <button
+                        type="submit"
+                        class="btn btn-info mb-1 mt-2 me-1">
+                        Create
+                    </button>
+                    <a
+                        href="{{ route('advertisement.index') }}"
+                        class="btn btn-info mb-1 mt-2 me-1">
+                        Back
+                    </a>
+                </form>
             </div>
-            <button
-                type="submit"
-                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                Create my advertisement
-            </button>
-        </form>
-
+        </div>
     </div>
-</div>
 @endsection
